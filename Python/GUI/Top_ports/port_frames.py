76c4b91ef7ca0e7387482c3190a8d3b9ec2af10a
TriangleMeteor <0966459924qpwo@gmail.com>
2023-02-25 18:21:04 +0800

    20230225 1821

--- Python/GUI/Top_ports/port_frames.py
+++ Python/GUI/Top_ports/port_frames.py
@@ -8,6 +8,7 @@ import queue
 global q
 global port_menu
 global port_GUI
+global com_INFO
 
 def port_frame(root_frame, q_):
     #### queue setting
@@ -22,6 +23,8 @@ def port_frame(root_frame, q_):
     port_GUI = tk.StringVar()
     port_GUI.set("請選擇序列埠")
     com_list_description = read_com_list(q)
+    if(len(com_list_description) == 0):
+        com_list_description.append("")
     print(com_list_description)
     global port_menu
     port_menu = tk.OptionMenu(Top_frame, port_GUI, *com_list_description)
@@ -30,16 +33,19 @@ def port_frame(root_frame, q_):
     refresh_But = ttk.Button(Top_frame, text="重新整理", command=port_refresh)        ##, command=port_refresh
     refresh_But.pack(side="left")
     #### port connect
-    com_connect = ttk.Button(Top_frame, text="連線")         ##, command=connect
+    com_connect = ttk.Button(Top_frame, text="連線", command=connect)         ##, command=connect
     com_connect.pack(side="left")
     return Top_frame
 
-def read_com_list(q_):
-    q_['GTS'].put('read com list')
-    while(q_['STG'].empty()):
+def read_com_list(q):
+    global com_INFO
+    F = {"Function":'read com list'}
+    q['GTS'].put(F)
+    while(q['STG'].empty()):
         pass
-    list = q_["STG"].get()
-    return list
+    com_INFO = q["STG"].get()
+    com_list = com_INFO['com description']
+    return com_list
 
 def port_refresh():
     global port_menu
@@ -49,3 +55,25 @@ def port_refresh():
     port_menu['menu'].delete(0, 'end')
     for i in new_com_list:
         port_menu['menu'].add_command(label=i, command=tk._setit(port_GUI, i))
+
+def connect():
+    global q
+    global port_GUI
+    global com_INFO
+    select_com = port_GUI.get() 
+    SN = com_INFO['com description'].index(select_com)
+    com_name_List = com_INFO['com name']
+    com_name = str(com_name_List[SN])
+    print(com_name)
+    tr = {"Function":'connect',
+          "com_name":com_name,
+          "BAUD_RATE":int(115200)
+          }
+    q['GTS'].put(tr)
+    while(q['STG'].empty()):
+        pass
+    re = q['STG'].get()
+    if(re['Success']):
+        messagebox.showinfo("連線成功", '連線成功')
+    else:
+        messagebox.showinfo("Error", re["exception"])
--- Python/Serial__/serrr.py
+++ Python/Serial__/serrr.py
@@ -22,15 +22,10 @@ def Ser_loop():
             receive = q['GTS'].get()
             function_SN = function_set(receive)
             if(function_SN==1):
-                global ser
-                com_list = serial.tools.list_ports_windows.comports()
-                com_description = []
-                for p in com_list:
-                    com_description.append(p.description)
-                q['STG'].put(com_description)
+                Function_1(receive)
+            if(function_SN==2):
+                Function_2(receive)
     
-
-
         time.sleep(0.002)
         if(q["STOP"].empty()==False):
             non_stop = False
@@ -40,10 +35,43 @@ def Ser_loop():
 
 def function_set(receive):
     function = 0
-    ## string
-    if(isinstance(receive, str)):
-        if(receive=="read com list"):
-            function = 1
-            return(function)
+    if(receive["Function"]=="read com list"):
+        function = 1
+        return(function)
+    if(receive["Function"]=="connect"):
+        function = 2
+        return function
 
+def Function_1(receive):
+    global ser
+    global q
+    com_list = serial.tools.list_ports_windows.comports()
+    com_description = []
+    com_name = []
+    for p in com_list:
+        com_description.append(p.description)
+        com_name.append(p.name)
+    tr = {
+        "command SN": 1,
+        "com description": com_description,
+        "com name": com_name
+    }
+    q['STG'].put(tr)
+
+def Function_2(receive):
+    global ser
+    global q
+    excep = ""
+    success = False
+    try:
+        ser = serial.Serial(receive["com_name"], receive["BAUD_RATE"])
+        success = True
+    except Exception as ee:
+        excep = ee
+        print(ee)
+    tr = {"command SN": 2,
+           "Success": success,
+           "exception":excep
+           }
+    q['STG'].put(tr)
     
